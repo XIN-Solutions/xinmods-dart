578e17d810bd7874496465520b70e21833261670
Marnix Kok <marnix@xinsolutions.co.nz>
2024-06-12 23:28:56 +1200

    Move more services over

---
+++ lib/src/connection_service.dart
@@ -0,0 +1,29 @@
+import 'package:xinmods/xinmods.dart';
+
+import "xm_auth_headers_stub.dart"
+  if (dart.library.io) 'xinmods/xm_auth_headers_basic.dart'
+  if (dart.library.html) 'xinmods/xm_auth_headers_web.dart'
+;
+
+import 'package:flutter/foundation.dart';
+
+XmConnection getXmConnection() {
+
+  if (kIsWeb) {
+    var conn = getCustomConnection();
+
+    if (conn != null) {
+      return conn;
+    }
+  }
+
+  // fallback connection (useful for when running a Linux app locally)
+  return XmConnection(
+    connectionType: XinmodsConnectionType.credentials,
+    deploymentType: XinmodsDeploymentType.local,
+    url: "http://localhost:8080",
+    user: "admin",
+    password: "admin"
+  );
+}
+
